0e348aa5fd39bb3289b2389de11b6f413559c9e2
renomureza <renomureza@gmail.com>
2021-06-09 09:53:57 +0700

    update vercel config, add cors, update route

--- src/index.js
+++ src/index.js
@@ -3,13 +3,39 @@ const app = require('express')();
 const weatherRoute = require('./routes/weather');
 const quakeRoute = require('./routes/quake');
 const responseCreator = require('./utils/responseCreator');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate=59');
+  next();
+});
 
 app.use('/weather', weatherRoute);
 app.use('/quake', quakeRoute);
 
-app.use('*', (req, res) => {
+app.get('/', (req, res) => {
+  return res.status(200).send({
+    maintainer: 'Renova Muhamad Reza',
+    source: 'https://github.com/renomureza/cuaca-gempa-rest-api',
+    endpoint: {
+      quake: `${process.env.BASE_URL}/quake`,
+      weather: {
+        province: {
+          example: `${process.env.BASE_URL}/weather/jawa-barat`,
+        },
+        city: {
+          example: `${process.env.BASE_URL}/weather/jawa-barat/bandung`,
+        },
+      },
+    },
+  });
+});
+
+app.all('*', (req, res) => {
   return res.status(404).send(responseCreator({ message: 'Not found' }));
 });
 
