184b6673f529a688cdf96ca12228880f67932cef
Izhur <izhur2001@gmail.com>
2022-11-16 18:24:26 +0700

    feat: add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './views/**/*.hbs',
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
